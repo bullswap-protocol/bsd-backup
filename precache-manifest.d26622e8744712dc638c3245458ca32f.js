2b7c307ffe1fc271362abb140581586dcfce82bb
bullswap-protocol <support@bullswap.org>
2020-12-31 17:06:23 +0700

    init

---
+++ precache-manifest.d26622e8744712dc638c3245458ca32f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec94c54d436c2cc37531205976770969",
+    "url": "/bsd-backup/index.html"
+  },
+  {
+    "revision": "c4c5219325cb065c8cd9",
+    "url": "/bsd-backup/static/css/2.76ef3db6.chunk.css"
+  },
+  {
+    "revision": "855c8bb0f5f1af6b2e1f",
+    "url": "/bsd-backup/static/css/main.077c0737.chunk.css"
+  },
+  {
+    "revision": "c4c5219325cb065c8cd9",
+    "url": "/bsd-backup/static/js/2.adba30cd.chunk.js"
+  },
+  {
+    "revision": "589fbfc2eb9d1a39c1a65f8041ae5e7a",
+    "url": "/bsd-backup/static/js/2.adba30cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855c8bb0f5f1af6b2e1f",
+    "url": "/bsd-backup/static/js/main.7803e126.chunk.js"
+  },
+  {
+    "revision": "20ab0ab7c82fa394e6c8",
+    "url": "/bsd-backup/static/js/runtime-main.6c5bdde4.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/bsd-backup/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/bsd-backup/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/bsd-backup/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/bsd-backup/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/bsd-backup/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
